15ebccb961e6fe76f89b81e68b16ef68dc9d4c18
Albert <>
2018-10-01 15:14:50 -0400

    RMI version bug fixed

--- Server/MiddlewareServer/Common/ResourceManager.java
+++ Server/MiddlewareServer/Common/ResourceManager.java
@@ -21,58 +21,20 @@ public class ResourceManager implements IResourceManager
 	protected RMHashMap m_data = new RMHashMap();
     private static String s_rmiPrefix = "group15";
 
-    private static String s_serverHost_f = "localhost";
-    private static int s_serverPort_f = 1099;
-    private static String s_serverName_f = "Flights";
-
-    private static String s_serverHost_c = "localhost";
-    private static int s_serverPort_c = 1099;
-    private static String s_serverName_c = "Cars";
-
-    private static String s_serverHost_r = "localhost";
-    private static int s_serverPort_r = 1099;
-    private static String s_serverName_r = "Rooms";
-
-	private static String s_serverHost_cus = "localhost";
-	private static int s_serverPort_cus = 1099;
-	private static String s_serverName_cus = "Customers";
+	public IResourceManager m_resourceManager_f = null;
+	public IResourceManager m_resourceManager_c = null;
+	public IResourceManager m_resourceManager_r = null;
+	public IResourceManager m_resourceManager_cus = null;
 
 	public ResourceManager(String p_name)
 	{
 		m_name = p_name;
 	}
 
-	IResourceManager m_resourceManager_f = null;
-	IResourceManager m_resourceManager_c = null;
-	IResourceManager m_resourceManager_r = null;
-	IResourceManager m_resourceManager_cus = null;
 
-	public boolean connect(){
-		if (System.getSecurityManager() == null)
-		{
-			System.setSecurityManager(new SecurityManager());
-		}
-		try {
-			if(m_resourceManager_f== null)
-				connectServer(s_serverHost_f, s_serverPort_f, s_serverName_f, m_resourceManager_f);
-			if(m_resourceManager_c== null)
-				connectServer(s_serverHost_c, s_serverPort_c, s_serverName_c, m_resourceManager_c);
-			if(m_resourceManager_r== null)
-				connectServer(s_serverHost_r, s_serverPort_r, s_serverName_r, m_resourceManager_r);
-			if(m_resourceManager_cus== null)
-				connectServer(s_serverHost_cus, s_serverPort_cus, s_serverName_cus, m_resourceManager_cus);
-		}
-		catch (Exception e) {
-			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUncaught exception");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return true;
-	}
-
-
-	public void connectServer(String server, int port, String name,IResourceManager m_resourceManager)
+	public IResourceManager connectServer(String server, int port, String name)
     {
+		IResourceManager m_resourceManager;
         try {
             boolean first = true;
             while (true) {
@@ -80,7 +42,7 @@ public class ResourceManager implements IResourceManager
                     Registry registry = LocateRegistry.getRegistry(server, port);
                     m_resourceManager = (IResourceManager) registry.lookup(s_rmiPrefix + name);
                     System.out.println("Connected to '" + name + "' server [" + server + ":" + port + "/" + s_rmiPrefix + name + "]");
-                    break;
+                    return m_resourceManager;
                 }
                 catch (NotBoundException|RemoteException e) {
                     if (first) {
@@ -96,6 +58,7 @@ public class ResourceManager implements IResourceManager
             e.printStackTrace();
             System.exit(1);
         }
+        return null;
     }
 
 
@@ -104,7 +67,7 @@ public class ResourceManager implements IResourceManager
 	public boolean addFlight(int xid, int flightNum, int flightSeats, int flightPrice) throws RemoteException
 	{
 		Trace.info("RM::addFlight(" + xid + ", " + flightNum + ", " + flightSeats + ", $" + flightPrice + ") called");
-		connect();
+//		connect();
 		return m_resourceManager_f.addFlight(xid, flightNum, flightSeats, flightPrice);
 	}
 
@@ -113,9 +76,7 @@ public class ResourceManager implements IResourceManager
 	public boolean addCars(int xid, String location, int count, int price) throws RemoteException
 	{
 		Trace.info("RM::addCars(" + xid + ", " + location + ", " + count + ", $" + price + ") called");
-
-        connect();
-		return m_resourceManager_c.addCars(xid, location, count, price);
+		return  m_resourceManager_c.addCars(xid, location, count, price);
 	}
 
 	// Create a new room location or add rooms to an existing location
@@ -123,116 +84,100 @@ public class ResourceManager implements IResourceManager
 	public boolean addRooms(int xid, String location, int count, int price) throws RemoteException
 	{
 		Trace.info("RM::addRooms(" + xid + ", " + location + ", " + count + ", $" + price + ") called");
-        connect();
 		return m_resourceManager_r.addRooms(xid, location, count, price);
 	}
 
 	// Deletes flight
 	public boolean deleteFlight(int xid, int flightNum) throws RemoteException
 	{
-        connect();
 		return m_resourceManager_f.deleteFlight(xid, flightNum);
 	}
 
 	// Delete cars at a location
 	public boolean deleteCars(int xid, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_c.deleteCars(xid, location);
 	}
 
 	// Delete rooms at a location
 	public boolean deleteRooms(int xid, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_r.deleteRooms(xid, location);
 	}
 
 	// Returns the number of empty seats in this flight
 	public int queryFlight(int xid, int flightNum) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_f.queryFlight(xid, flightNum);
 	}
 
 	// Returns the number of cars available at a location
 	public int queryCars(int xid, String location) throws RemoteException
 	{
-		connect();
+//		connect();
 		return m_resourceManager_c.queryCars(xid, location);
 	}
 
 	// Returns the amount of rooms available at a location
 	public int queryRooms(int xid, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_r.queryRooms(xid, location);
 	}
 
 	// Returns price of a seat in this flight
 	public int queryFlightPrice(int xid, int flightNum) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_f.queryFlightPrice(xid, flightNum);
 	}
 
 	// Returns price of cars at this location
 	public int queryCarsPrice(int xid, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_c.queryCarsPrice(xid, location);
 	}
 
 	// Returns room price at this location
 	public int queryRoomsPrice(int xid, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_r.queryRoomsPrice(xid, location);
 	}
 
 	public String queryCustomerInfo(int xid, int customerID) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_cus.queryCustomerInfo(xid, customerID);
 	}
 
 	public int newCustomer(int xid) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_cus.newCustomer(xid);
 	}
 
 	public boolean newCustomer(int xid, int customerID) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_cus.newCustomer(xid,customerID);
 	}
 
 	public boolean deleteCustomer(int xid, int customerID) throws RemoteException
 	{
 		Trace.info("RM::deleteCustomer(" + xid + ", " + customerID + ") called");
-		connect();
 		return m_resourceManager_cus.deleteCustomer(xid,customerID);
 	}
 
 	// Adds flight reservation to this customer
 	public boolean reserveFlight(int xid, int customerID, int flightNum) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_f.reserveFlight(xid, customerID, flightNum);
 	}
 
 	// Adds car reservation to this customer
 	public boolean reserveCar(int xid, int customerID, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_c.reserveCar(xid, customerID, location);
 	}
 
 	// Adds room reservation to this customer
 	public boolean reserveRoom(int xid, int customerID, String location) throws RemoteException
 	{
-		connect();
 		return m_resourceManager_r.reserveRoom(xid, customerID, location);
 	}
 
--- Server/MiddlewareServer/RMI/RMIResourceManager.java
+++ Server/MiddlewareServer/RMI/RMIResourceManager.java
@@ -21,6 +21,23 @@ public class RMIResourceManager extends ResourceManager
 	private static String s_serverName = "MiddlewareServer";
 	private static String s_rmiPrefix = "group15";
 
+
+	private static String s_serverHost_f = "localhost";
+	private static int s_serverPort_f = 1099;
+	private static String s_serverName_f = "Flights";
+
+	private static String s_serverHost_c = "localhost";
+	private static int s_serverPort_c = 1099;
+	private static String s_serverName_c = "Cars";
+
+	private static String s_serverHost_r = "localhost";
+	private static int s_serverPort_r = 1099;
+	private static String s_serverName_r = "Rooms";
+
+	private static String s_serverHost_cus = "localhost";
+	private static int s_serverPort_cus = 1099;
+	private static String s_serverName_cus = "Customers";
+
 	public static void main(String args[])
 	{
 		if (args.length > 0)
@@ -29,10 +46,9 @@ public class RMIResourceManager extends ResourceManager
 		}
 
 		// Create the RMI server entry
+		// Create a new Server object
+		RMIResourceManager server = new RMIResourceManager(s_serverName);
 		try {
-			// Create a new Server object
-			RMIResourceManager server = new RMIResourceManager(s_serverName);
-
 			// Dynamically generate the stub (client proxy)
 			IResourceManager resourceManager = (IResourceManager)UnicastRemoteObject.exportObject(server, 0);
 
@@ -71,10 +87,39 @@ public class RMIResourceManager extends ResourceManager
 		{
 			System.setSecurityManager(new SecurityManager());
 		}
+
+		try {
+			server.connect();
+		}
+		catch (Exception e) {
+			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUncaught exception");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 	public RMIResourceManager(String name)
 	{
 		super(name);
+	}
+
+
+	public boolean connect(){
+		if (System.getSecurityManager() == null)
+		{
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			m_resourceManager_f = connectServer(s_serverHost_f, s_serverPort_f, s_serverName_f);
+			m_resourceManager_c = connectServer(s_serverHost_c, s_serverPort_c, s_serverName_c);
+			m_resourceManager_r = connectServer(s_serverHost_r, s_serverPort_r, s_serverName_r);
+			m_resourceManager_cus = connectServer(s_serverHost_cus, s_serverPort_cus, s_serverName_cus);
+		}
+		catch (Exception e) {
+			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUncaught exception");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return true;
 	}
 }
