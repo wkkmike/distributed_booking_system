86763fd1c649c12ad0a6aa3375d123a2f574cd5e
wkkmike <michaelwang404@hotmail.com>
2018-11-04 18:03:05 -0500

    Add redo operation for reservation for customer.

--- Server/Server/Common/Customer.java
+++ Server/Server/Common/Customer.java
@@ -46,6 +46,27 @@ public class Customer extends RMItem
 		m_reservations.put(reservedItem.getKey(), reservedItem);
 	}
 
+	public boolean unReserve(String key, String location, int price)
+	{
+		ReservedItem reservedItem = getReservedItem(key);
+		if(reservedItem == null){
+			// Customer don't have this item in bill, something wrong.
+			return false;
+		}
+		else{
+			reservedItem.setCount(reservedItem.getCount() - 1);
+			reservedItem.setPrice(price);
+		}
+		if(reservedItem.getCount() == 0){
+			// Remove the item from customer reservation, if the count is 0;
+			m_reservations.remove(reservedItem.getKey());
+		}
+		else{
+			m_reservations.put(reservedItem.getKey(), reservedItem);
+		}
+		return true;
+	}
+
 	public ReservedItem getReservedItem(String key)
 	{
 		return (ReservedItem)m_reservations.get(key);
